e399d48fe1e2b623855720dea0d85169911d8e8b
comye1 <50735594+comye1@users.noreply.github.com>
2020-05-22 23:44:25 +0900

    CustomAdapter -> CustomViewHolder

--- FirebaseListExample/app/src/main/java/com/example/firebaselistexample/CustomAdapter.java
+++ FirebaseListExample/app/src/main/java/com/example/firebaselistexample/CustomAdapter.java
@@ -1,15 +1,34 @@
 package com.example.firebaselistexample;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
+    private ArrayList<User> arrayList;
+    private Context context;
+
+    public CustomAdapter(ArrayList<User> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+
+        return holder;
     }
 
     @Override
@@ -22,6 +41,19 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         return 0;
     }
 
-    public class CustomViewHolder {
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView_profile;
+        TextView textView_id;
+        TextView textView_pw;
+        TextView textView_userName;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView_profile = itemView.findViewById(R.id.imageView_profile);
+            this.textView_id = itemView.findViewById(R.id.textView_id);
+            this.textView_pw = itemView.findViewById(R.id.textView_pw);
+            this.textView_userName = itemView.findViewById(R.id.textView_userName);
+
+        }
     }
 }
